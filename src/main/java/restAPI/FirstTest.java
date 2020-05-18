7bc29520379aeeb412e0e58471491d54e7b081cc
NRK <NRK@169.254.147.79>
2020-05-18 21:26:10 +0530

    First commit

---
+++ src/main/java/restAPI/FirstTest.java
@@ -0,0 +1,9 @@
+package restAPI;
+import static io.restassured.RestAssured.given;
+
+public class FirstTest {
+public static void main(String[] args) {
+	System.out.println("Hello");
+	//given().
+}
+}
